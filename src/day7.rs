9d6c43827071381e9afec94d56a9814167bdad0d
mattherix <mattherix@protonmail.com>
2022-12-08 22:02:47 -0500

    feat(2022/day7): solve second part

--- src/day7.rs
+++ src/day7.rs
@@ -36,6 +36,7 @@ pub fn solve<P: AsRef<Path>>(path: P) -> (u32, u32) {
 
     let mut directory_size = vec![];
     let mut directory_size_stack = vec![];
+    let mut space_used: u32 = 0;
     for instruction in data {
         match instruction[0] {
             "cd" => {
@@ -55,16 +56,24 @@ pub fn solve<P: AsRef<Path>>(path: P) -> (u32, u32) {
             "ls" => {
                 let mut last = directory_size_stack.last_mut().unwrap();
                 
-                *last += instruction
+                let files_size = instruction
                     .iter()
                     .filter_map(|s| s.parse::<u32>().ok())
                     .sum::<u32>();
+
+                space_used += &files_size;
+                *last += files_size
             },
             _ => panic!("Can't execute instruction {:?}", instruction)
         }
     }
     
     while let Some(size) = directory_size_stack.pop() {
+        let mut last = directory_size_stack.last_mut();
+        if let Some(last) = last {
+            *last += size;
+        }
+
         directory_size.push(size);
     }
     
@@ -72,11 +81,21 @@ pub fn solve<P: AsRef<Path>>(path: P) -> (u32, u32) {
         .iter()
         .filter(|val| **val <= 100_000)
         .sum();
+    
+    let free_space = 70_000_000 - space_used as i32;
+    let space_to_free = 30_000_000 - free_space;
+    
+    let mut mini = std::u32::MAX;
+    for directory in directory_size {
+        if directory >= space_to_free.try_into().unwrap() && directory <= mini {
+            mini = directory;
+        }
+    }
 
-    (part1, 0)
+    (part1, mini)
 }
 
 #[test]
-fn feature() {
-    assert_eq!(solve("inputs/day7_test.txt"), (95437, 0));
+fn test_example_input() {
+    assert_eq!(solve("inputs/day7_test.txt"), (95437, 24933642));
 }
